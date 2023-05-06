0bb3cab92662ae3d9ca058ca7518f5b11b60168a
David williams pyrrho de lima <78514902+davidmidoria@users.noreply.github.com>
2023-05-06 13:22:21 -0300

    listas padrão do código
    começando pelas listas onde são armazenadas as notas dos candidatos e as categorias que foram avaliadas

---
+++ arquivo do apk de compatibilidade/apkDeCompatibilidade.py
@@ -0,0 +1,2 @@
+candidatos=[["allan harlem"[5,10,8,8]],['michel farias'[10,7,7,8]],['thiago silva'[8,5,4,9]],['gabriel ramos'[2,2,2,1]],['dayane stefani'[10,10,8,9]]]
+categorias=['entrevista:','teste téorico','teste prático','avaliação de soft skills']
