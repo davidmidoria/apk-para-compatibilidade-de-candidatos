bd0532ecc8038c757e76cb2b83586282bf09de1c
David williams pyrrho de lima <78514902+davidmidoria@users.noreply.github.com>
2023-05-16 16:01:02 -0300

    uma serie de melhorias
    cadastro se torna menos suscetível a erros
    
    há uma diminuição no número de funções
    
    código se torna um pouco mais enxuto

--- arquivo do apk de compatibilidade/apkDeCompatibilidade.py
+++ arquivo do apk de compatibilidade/apkDeCompatibilidade.py
@@ -1,45 +1,42 @@
-def recebedorDeNotas (mensagem='digite a nota:',notaMaxima=10):
-    resposta=''
-    while type(resposta) !=float or resposta>notaMaxima:
+# inputsNumericos: recebe duas variaves um do conteudo do input() é o segundo do maior número que se quer ser recebido
+# retorna um número de ponto flutuante
+def inputsNumericos (mensagem='digite:',maiorInputPossivel=10):
         try:
          resposta=(float(input(mensagem)))
-         if resposta>notaMaxima:
-            print(f'escreva um valor numerico entre 0 e {notaMaxima}')
-        except:
-           print('por favor digite um valor numerico')
-    return resposta
-# recebedorDeNotas: essa função possui dois parâmetros que são opcionais  a 'primeira' é a mensagem que é o valor quê será impresso no terminal para recebimento do valor escrito pelo cliente  é a segunda 'notaMaxima' que como o nome ja desreve é a maior nota que pode ser escrita pelo usuario. basicamente essa função e projetada para retornar um valor numerico no formato 'float'. 
+        except:resposta=maiorInputPossivel+1
+        if  resposta<=maiorInputPossivel:
+            return resposta
+        else:
+            print('digite um valor numerico entre 0 e {}'.format(maiorInputPossivel))
+            inputsNumericos(maiorInputPossivel=maiorInputPossivel)
 
-def padraoMinimo(textoBase,categorias):
-    print(textoBase)
-    padrao=[]
-    for categoria in categorias:
-        padrao.append(recebedorDeNotas(categoria))
-    return padrao
+# valorItem recebe um texto para exibir no terminal é uma lista de itens, retorna uma lista com um valor para cada item 
+def valorItem(texto,itens):
+    print(texto)
+    valosDosItens=[]
+    for categoria in itens:
+        valosDosItens.append(inputsNumericos(categoria))
+    return valosDosItens
 
-#padraoMinimo: responsavel por escrever o texto base é armazenar a nota dada a diferentes categorias de dados, recebe texto base que funciona como um cabeçalho e categorias quê deve ser uma lista que sera iterada afim de que o usuario possa dar a nota como base o item especifico da lista, a função armazena uma nova lista com o nome de padrao que será o retorno dado pela função
-
-def testeaprovacao(lista0,lista1):
+#comparaLista: faz uma comparação entre duas listas, retorna o número de vezes que os itens da lista0 foram maiores que o da lista1
+def comparaLista(lista0,lista1):
     contador=0
     for item0,item1 in zip(lista0,lista1):
      if item0>=item1:
         contador+=1
-    if contador==len(lista0):
-       return True
-    else:
-       return False
+    return contador
 
-#testeaprovacao: essa função verifica se todos os itens da lista 0 são maiores ou iguais ao da lista 
+#comparadorDeListas: recebe uma lista aninhada é uma segunda lista modelo, retorna uma lista com as listas em que todos os itens são maiores
+#que na lista modelo
+def comparadorDeListas(listasaninhada,listamodelo):
+   maioresQueModelo=[]
+   for indice in range(len(listasaninhada)):
+        if comparaLista(listasaninhada[indice][1],listamodelo)==len(listamodelo):
+            maioresQueModelo.append(listasaninhada[indice])
+   return maioresQueModelo
 
-def testeDelista(emTexte,modelo):
-   aprovado=[]
-   for indice in range(len(emTexte)):
-        if testeaprovacao(emTexte[indice][1],modelo)==True:
-            aprovado.append(emTexte[indice])
-   return aprovado
-
-#função testeDeLista essa função faz uma iteração de listas recebida no parametro 'emtexte' e mede se os itens contidos nestas listas são todos maiores ou iguais aos quê estão na lista 'modelo', se todos os itens da lista forem maiores ou iguais a lista que contém a lista testada e devolvida  pela lista 'aprovado' na função return. 
-
+#listaDeaprovado recebe uma listas que deve conter uma ou mais listas em um formato especifico que vai imprimir uma mensagem com nome é as 
+# notas dos aprovados
 def listaDeaprovado(aprovados):
    if len(aprovados)>0:
       print("""os candidatos aptos para a vaga são:""")
@@ -48,87 +45,52 @@ def listaDeaprovado(aprovados):
    else:
        print('não ha candidatos aptos')
 
-# recebe uma lista, se a lista conter uma ou mais listas sera escrito a lista com os candidatos aprovados caso não a mensagem 'não ha candidatos aptos'
-
-def tramento(notas):
+#inputPadrao: recebe um texto que sera impresso, uma estrutura padrao e um maior número para o retorno, retorna 
+# uma lista com números de ponto flutuante com valor maximo igual ao parametro maior numero
+def inputPadrao(texto,estruturaPadrao,maiorNumero=10):
     listnotas=[]
+    estrutura=[]
+    notas=input(texto).strip().lower().replace('_',' ').split()
     for i in notas:
-        if i.isnumeric():
-            listnotas.append(int(i))
-    return listnotas
+        try:
+            if float(i[1:])<=maiorNumero:
+                listnotas.append(float(i[1:]))
+                estrutura.append(i[:1])
+        except:
+            estrutura=[]
+    if estruturaPadrao==estrutura:
+        return listnotas
+    else:
+        return inputPadrao(texto,estruturaPadrao)
 
-# recebe uma string, separa os numeros da string  e retorna os numeros no formato de lista
-
-def cadastro():
-    nome=input('menu de cadastro\ndigite o nome do candidato:')
-    notas=tramento(input('informe as notas do texte no formato "eX_tX_pX_sX" sendo x o valor da nota\ndigite:'))
+# cadastro recebe uma lista contendo uma serie de informaçoes como estrutura padrão, txtnome, txtnotas retorna uma string é uma sequencia
+#númerica
+def cadastro(txtcadastro):
+    estruturapadrao,txtnome,txtnotas=txtcadastro
+    nome=input(txtnome)
+    notas=inputPadrao(txtnotas,estruturapadrao)
     return [nome,notas]
 
-# cria uma lsta contendo um nome é uma lista com as notas do candidato
+candidatos=[]# local onde se armazena as informações do candidato 
 
-def simOuNao(mensagem):
-    resposta=input(mensagem)
-    if resposta=='1':
-        return True
-    if resposta=='0':
-        return False
-    else:
-        print('digite 1 ou 0')
-        return simOuNao()
+categorias=['entrevista:','teste téorico:','teste prático:','avaliação de soft skills:']# categorias do teste
 
-# cria um looping caso o cliente digite algo diferente de 1 e 0 se for escrito 1 retorna True caso 0 retorna False
+textoBase="filtro de candidatos\n\ndescreva com notas entre 0 a 10 o padrão minimo para obtenção da vaga"# texto base do filtro de candidatos
 
-def menu(texto=''):
-    print(texto)
-    resposta=input('digite:')
-    if resposta=='0':
-        return False,False,False
-    elif resposta =='1':
-        return True,True,False
-    elif resposta=='2':
-        return True,False,True
-    else:
-        print('digite um número entre um e três')
-        return menu()
+textoMenu=" cadastro e compatibilidade\nmenu inicial\n1-cadastrar funcionarios\n2-verificar compatibilidade\n0-sair\ndigite:"#texto do menu
 
-# imprime um texto na tela e recebe um input() que devera ser um número entre 0 e 2 que retorna uma sequência de true e false true especifica para cada resposta. caso não for entra em looping atê que seja adcionada uma resposta valida.
+txtcadastro=[['e','t','p','s'],'menu de cadastro\ndigite o nome do candidato:','informe as notas do'
+              'teste no formato "eX_tX_pX_sX" sendo x o valor da nota, que não deve ser maior que 10\ndigite:' ]#uma lista que contém as
+#informações necessarias para o cadastro
 
-candidatos=[]
-#candidatos:responsavel pelo nome é notas dos candidatos, as notas dos candidatos seguem a mesma ordem da quê sera listada na variavel 'categorias'.
+loop=None
+while loop!=0:
+    loop=inputsNumericos(textoMenu,2)#menu
 
-categorias=['entrevista:','teste téorico:','teste prático:','avaliação de soft skills:']
-# categorias:Contém todas as categorias que foram usadas como parâmetros para a contratação do candidato
-  
+    while loop==1:
+        candidatos.append(cadastro(txtcadastro))
+        loop= ( None if inputsNumericos('\n1-novo cadastro\n0-retornar\ndigite:',1)==0 else 1)#cadastro
 
-textoBase="""filtro de candidatos
-
-descreva com notas entre 0 a 10 o padrão minimo para obtenção da vaga"""
-# texto base do filtro dos funcionarios
-
-textoMenu=""" 
-cadastro e compatibilidade
-menu inicial
-1-cadastrar funcionarios
-2-verificar compatibilidade
-0-sair
-"""
-#texto do menu
-
-
-loop0=True
-while loop0==True:
-    loop0,loop1,loop2=menu(textoMenu)
-
-    # inicia um while  responsavel por iniciar o funcionamento do programa, na linha 120 as variaveis responsaveis pelos loopings do codigo recebe uma lista contendo Trues e falses que vão definir qual função deve ser chamada seja a de cadastro ou a de listaDeaprovado 
-
-    while loop1==True:
-        candidatos.append(cadastro())
-        loop1= simOuNao('\n1-novo cadastro\n0-retornar\ndigite:')
-
-        #cadastra os usuarios a partir da função cadastro, verifica se um novo candidato deve ou não ser cadastro ,retorna ao inicio do looping principal. 
-    
-    while loop2==True:
-        listaDeaprovado(testeDelista(candidatos,padraoMinimo(textoBase,categorias)))
-        loop2=simOuNao('\n1-nova consulta\n0-retornar\ndigite:')
-
-    # cria uma lista com padrões minimos para os candidatos, compara com as listas presentes na lista candidatos, imprime os candidatos aptos caso eles existam  
+    while loop==2:
+        listaDeaprovado(comparadorDeListas(candidatos,valorItem(textoBase,categorias)))# parte que verifica a compatibilidade de um candidato.
+        loop=(2 if inputsNumericos('\n1-nova consulta\n0-retornar\ndigite:',1)==1 else None)
